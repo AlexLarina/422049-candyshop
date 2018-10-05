74cbe689d8a98c83340e396c14a78335cf6f34df
Alexandra Larina <aa.larina.ff@gmail.com>
2018-10-05 15:53:20 +0300

    Реализована логика слайдера

--- js/goods.js
+++ js/goods.js
@@ -455,7 +455,7 @@ var disableInputs = function (element) {
   element.querySelectorAll('input').forEach(function (it) {
     it.setAttribute('disabled', 'disabled');
   });
-}
+};
 /**
  * Делаем активными все интупы в выбранном блоке
  * @param {Node} element
@@ -464,7 +464,7 @@ var enableInputs = function (element) {
   element.querySelectorAll('input').forEach(function (it) {
     it.removeAttribute('disabled');
   });
-}
+};
 /**
  * По умолчанию инпуты в блкое курьеской доставки выключены
  */
@@ -516,16 +516,83 @@ chooseCardInput.addEventListener('click', function () {
  * @type {Element}
  */
 var leftRangeButton = document.querySelector('.range__btn--left');
-leftRangeButton.addEventListener('click', function () {
-  document.querySelector('.range__price--min').textContent = Math.floor(leftRangeButton.offsetLeft * 100 /
-    leftRangeButton.parentNode.offsetWidth);
-});
 var rightRangeButton = document.querySelector('.range__btn--right');
-rightRangeButton.addEventListener('click', function () {
-  document.querySelector('.range__price--max').textContent = Math.floor(rightRangeButton.offsetLeft * 100 /
-    rightRangeButton.parentNode.offsetWidth);
-});
+/**
+ * Обработка перетаскивания левого пина слайдера
+ */
+leftRangeButton.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var fillLine = document.querySelector('.range__fill-line');
+  var startX = evt.clientX;
 
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shiftX = startX - moveEvt.clientX;
+    startX = moveEvt.clientX;
+    var currentX = leftRangeButton.offsetLeft - shiftX;
+    var rightButtonX = rightRangeButton.offsetLeft;
+
+    if (currentX < 0) {
+      currentX = 0;
+    }
+    if (currentX > rightButtonX - 10) {
+      currentX = rightButtonX - 10;
+    }
+
+    leftRangeButton.style.left = currentX + 'px';
+    fillLine.style.left = currentX + 'px';
+    document.querySelector('.range__price--min').textContent = Math.floor(currentX * 100 /
+      leftRangeButton.parentNode.offsetWidth);
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+/**
+ * Обработка перетаскивания правого пина слайдера
+ */
+rightRangeButton.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var filter = document.querySelector('.range__filter');
+  var fillLine = document.querySelector('.range__fill-line');
+  var rightBorder = filter.offsetWidth;
+  var startX = evt.clientX;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shiftX = startX - moveEvt.clientX;
+    startX = moveEvt.clientX;
+    var currentX = rightRangeButton.offsetLeft - shiftX;
+    var leftButtonX = leftRangeButton.offsetLeft;
+
+    if (currentX > rightBorder) {
+      currentX = rightBorder;
+    }
+    if (currentX < leftButtonX + 10) {
+      currentX = leftButtonX + 10;
+    }
+
+    rightRangeButton.style.left = currentX + 'px';
+    fillLine.style.right = fillLine.parentNode.offsetWidth - currentX + 'px';
+    document.querySelector('.range__price--max').textContent = Math.floor(currentX * 100 /
+      rightRangeButton.parentNode.offsetWidth);
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
 /**
  * Валидация электронной почты
  * @param {string} email
@@ -666,7 +733,7 @@ var changeCardStatus = function () {
   if (isCardNumberValid && isCardDateValid && isCardCVCValid && isCardNameValid) {
     cardStatus.textContent = 'Одобрен';
   }
-}
+};
 /**
  * Обработка ввода данных в поле Этаж
  * @type {Element}
